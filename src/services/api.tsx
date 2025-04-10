82a8d32625502a9b1bdd2a95b9f8ba507d635972
Diana <bu_di_2005@mail.ru>
2025-04-10 09:20:28 +0300

    Typification axios.get

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,12 +1,21 @@
 import axios from 'axios';
 
-export const fetchCurrencies = async () => {
+type AxiosGet = {
+  conversion_rates:{
+    [key:string]:number;
+  }
+}
+
+const exchangeRates = 'exchangeRates';
+const exchangeRatesTime = 'exchangeRatesTime';
+
+export const fetchCurrencies = async (): Promise<{[key:string]:number}> => {
   try {
-    const response = await axios.get(import.meta.env.VITE_API_URL);
+    const response = await axios.get<AxiosGet>(import.meta.env.VITE_API_URL);
     const rates = response.data.conversion_rates;
 
-    localStorage.setItem('exchangeRates', JSON.stringify(rates));
-    localStorage.setItem('exchangeRatesTime', String(new Date().getTime()));
+    localStorage.setItem(exchangeRates, JSON.stringify(rates));
+    localStorage.setItem(exchangeRatesTime, String(new Date().getTime()));
 
     return rates;
   } catch (error) {
